2a66b09dfa2874357b30d08b4f61fcd6247f45c1
RikaFujiwara <rika.fujiwara@allcreator.net>
2016-07-14 05:45:26 +0000

    phpunit errorが出ているところを突き止めるための作業７

---
+++ Test/Case/Controller/CalendarFrameSettings/EditTest.php
@@ -0,0 +1,308 @@
+<?php
+/**
+ * CalendarFrameSettingsController Test Case
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Allcreator <info@allcreator.net>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+App::uses('CalendarsComponent', 'Calendars.Controller/Component');
+App::uses('CalendarFrameSettingFixture', 'Calendars.Test/Fixture');
+App::uses('CalendarFrameSettingSelectRoomFixture', 'Calendars.Test/Fixture');
+
+/**
+ * CalendarFrameSettingsController Test Case
+ *
+ * @author Allcreator <info@allcreator.net>
+ * @package NetCommons\Calendars\Test\Case\Controller
+ */
+class CalendarsFrameSettingsControllerEditTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.calendars.calendar',
+		'plugin.calendars.calendar_event',
+		'plugin.calendars.calendar_event_content',
+		'plugin.calendars.calendar_event_share_user',
+		'plugin.calendars.calendar_frame_setting',
+		'plugin.calendars.calendar_frame_setting_select_room',
+		'plugin.calendars.calendar_rrule',
+		'plugin.workflow.workflow_comment',
+		'plugin.rooms.rooms_language4test',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var array
+ */
+	public $plugin = 'calendars';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'calendar_frame_settings';
+
+/**
+ * テストDataの取得
+ *
+ * @return array
+ */
+	private function __getData() {
+		//$frameId = '6';
+		//$blockId = '2';
+		$data['Frame']['id'] = 6;
+		$data['CalendarFrameSetting'] = (new CalendarFrameSettingFixture())->records[0];
+		$data['CalendarFrameSetting']['is_select_room'] = '1';
+
+		$data['CalendarFrameSettingSelectRoom'] = array();
+		$selectRoomFixture = new CalendarFrameSettingSelectRoomFixture();
+		// Modelの試験のときはパブリックデータしか操作できない....ログイン状態を作れない
+		$data['CalendarFrameSettingSelectRoom'][1] = $selectRoomFixture->records[0];
+		$data['CalendarFrameSettingSelectRoom'][4] = $selectRoomFixture->records[3];
+		$data['CalendarFrameSettingSelectRoom'][5] = array(
+			'calendar_frame_setting_id' => 1,
+			'room_id' => 5
+		);
+		return $data;
+	}
+
+/**
+ * editアクションのGETテスト
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderEditGet
+ * @return void
+ */
+	public function testEditGet($urlOptions, $assert, $exception = null, $return = 'view') {
+		//Exception
+		if ($exception) {
+			$this->setExpectedException($exception);
+		}
+
+		//テスト実施
+		$url = Hash::merge(array(
+			'plugin' => $this->plugin,
+			'controller' => $this->_controller,
+			'action' => 'edit',
+		), $urlOptions);
+
+		$this->_testGetAction($url, $assert, $exception, $return);
+	}
+/**
+ * editアクションのGETテスト(ログインなし)用DataProvider
+ *
+ * #### 戻り値
+ *  - urlOptions: URLオプション
+ *  - assert: テストの期待値
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderEditGet() {
+		$data = $this->__getData();
+		$results = array();
+
+		//ログインなし
+		$results[0] = array(
+			'urlOptions' => array('frame_id' => $data['Frame']['id']),
+			'assert' => null,
+			'exception' => 'ForbiddenException',
+		);
+		return $results;
+	}
+
+/**
+ * editアクションのGETテスト
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderEditGetByPublishable
+ * @return void
+ */
+	public function testEditGetByPublishable($urlOptions, $assert, $exception = null, $return = 'view') {
+		//ログイン
+		TestAuthGeneral::login($this, Role::ROOM_ROLE_KEY_ROOM_ADMINISTRATOR);
+
+		//テスト実施
+		$url = Hash::merge(array(
+			'plugin' => $this->plugin,
+			'controller' => $this->_controller,
+			'action' => 'edit',
+		), $urlOptions);
+
+		$this->_testGetAction($url, $assert, $exception, $return);
+
+		//ログアウト
+		TestAuthGeneral::logout($this);
+	}
+/**
+ * editアクションのGETテスト(ログインあり)用DataProvider
+ *
+ * #### 戻り値
+ *  - urlOptions: URLオプション
+ *  - assert: テストの期待値
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderEditGetByPublishable() {
+		$data0 = $this->__getData();
+		$results = array();
+
+		//ログインあり
+		$results[0] = array(
+			'urlOptions' => array('frame_id' => $data0['Frame']['id']),
+			'assert' => null
+		);
+		// ブロックが存在しないフレームID
+		$results[1] = array(
+			'urlOptions' => array('frame_id' => 15),
+			'assert' => null,
+		);
+		// 存在しないフレームID
+		$results[2] = array(
+			'urlOptions' => array('frame_id' => 9999),
+			'assert' => null,
+			'exception' => 'BadRequestException',
+		);
+		return $results;
+	}
+
+/**
+ * editアクションのPOSTテスト
+ *
+ * @param array $data POSTデータ
+ * @param string $role ロール
+ * @param array $urlOptions URLオプション
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderEditPost
+ * @return void
+ */
+	public function testEditPost($data, $role, $urlOptions, $exception = null, $return = 'view') {
+		//ログイン
+		if (isset($role)) {
+			TestAuthGeneral::login($this, $role);
+		}
+
+		//テスト実施
+		$this->_testPostAction('put', $data, Hash::merge(array('action' => 'edit'), $urlOptions), $exception, $return);
+
+		//正常の場合、リダイレクト
+		if (! $exception) {
+			$header = $this->controller->response->header();
+			$this->assertNotEmpty($header['Location']);
+		}
+
+		//ログアウト
+		if (isset($role)) {
+			TestAuthGeneral::logout($this);
+		}
+	}
+/**
+ * editアクションのPOSTテスト用DataProvider
+ *
+ * #### 戻り値
+ *  - data: 登録データ
+ *  - role: ロール
+ *  - urlOptions: URLオプション
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderEditPost() {
+		$data = $this->__getData();
+
+		return array(
+			//ログインなし
+			array(
+				'data' => $data, 'role' => null,
+				'urlOptions' => array('frame_id' => $data['Frame']['id']),
+				'exception' => 'ForbiddenException'
+			),
+
+			//正常
+			array(
+				'data' => $data, 'role' => Role::ROOM_ROLE_KEY_ROOM_ADMINISTRATOR,
+				'urlOptions' => array('frame_id' => $data['Frame']['id']),
+
+			),
+			//フレームID指定なしテスト
+			array(
+				'data' => $data, 'role' => Role::ROOM_ROLE_KEY_ROOM_ADMINISTRATOR,
+				'urlOptions' => array('frame_id' => null),
+
+			),
+
+		);
+	}
+
+/**
+ * editアクションのValidateionErrorテスト
+ *
+ * @param array $data POSTデータ
+ * @param array $urlOptions URLオプション
+ * @param string|null $validationError ValidationError
+ * @dataProvider dataProviderEditValidationError
+ * @return void
+ */
+	public function testEditValidationError($data, $urlOptions, $validationError = null) {
+		//ログイン
+		TestAuthGeneral::login($this);
+
+		//テスト実施
+		$this->_testActionOnValidationError('put', $data, Hash::merge(array('action' => 'edit'), $urlOptions), $validationError);
+
+		//ログアウト
+		TestAuthGeneral::logout($this);
+	}
+/**
+ * editアクションのValidationErrorテスト用DataProvider
+ *
+ * #### 戻り値
+ *  - data: 登録データ
+ *  - urlOptions: URLオプション
+ *  - validationError: バリデーションエラー
+ *
+ * @return array
+ */
+	public function dataProviderEditValidationError() {
+		$data = $this->__getData();
+
+		$result = array(
+			'data' => $data,
+			'urlOptions' => array('frame_id' => $data['Frame']['id']),
+		);
+
+		return array(
+			//バリデーションエラー
+			Hash::merge($result, array(
+				'validationError' => array(
+					'field' => 'CalendarFrameSetting.display_type',
+					'value' => '300000',
+					'message' => __d('net_commons', 'Invalid request.'),
+				)
+			)),
+		);
+	}
+
+}
