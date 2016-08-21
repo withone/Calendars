58472c3b5068ea31d49f5cccd771fcf3d6610d53
RikaFujiwara <rika.fujiwara@allcreator.net>
2016-08-21 02:48:49 +0000

    不要コード削除

--- Model/Behavior/CalendarRoleAndPermBehavior.php
+++ Model/Behavior/CalendarRoleAndPermBehavior.php
@@ -51,31 +51,6 @@ class CalendarRoleAndPermBehavior extends CalendarAppBehavior {
 	public function initSetting(Model &$model, $workflow) {
 		$this->__workflowCompo = $workflow;
 	}
-/**
- * isContentPublishableWithCalRoleAndPerm
- * 現在未使用 削除予定
- *
- * カレンダー権限管理を考慮したCurrentユーザの指定oomで付与された役割での承認権限の有無
- *
- * @param Model &$model 実際のモデル名
- * @param int $roomId roomId
- * @return array $readableRoomIds(参照可能room一覧), $roleOfRooms(ルームごとでの役割一覧)、$roomInfos(ルームでのルーム管理＋カレンダー権限管理での承認権限有無一覧), $rooms(ルームでの役割別権限一覧)を格納した配列を返す。
- */
-//	public function isContentPublishableWithCalRoleAndPerm(Model &$model, $roomId) {
-//		//カレンダー役割・権限の準備
-//		if ($this->_calRoleAndPerm === null) {
-//			$this->_calRoleAndPerm = $this->prepareCalRoleAndPerm($model);
-//		}
-//		$roomInfos = &$this->_calRoleAndPerm['roomInfos'];
-//
-//		$isContentPublishable = false;
-//		if ($roomInfos[$roomId]['use_workflow']) {
-//			if (!empty($roomInfos[$roomId]['content_publishable_value'])) {
-//				$isContentPublishable = true;
-//			}
-//		}
-//		return $isContentPublishable;
-//	}
 
 /**
  * prepareCalRoleAndPerm
@@ -226,106 +201,6 @@ class CalendarRoleAndPermBehavior extends CalendarAppBehavior {
 			'content_creatable_value' => $creatable
 		);
 	}
-
-/**
- * getRoleInRoom
- * 現在未使用関数となった
- *
- * Currentユーザの、指定ルームにおける役割(role_key)を取得
- *
- * @param Model &$model 実際のモデル名
- * @param array $calRoleAndPerm カレンダー用役割・権限格納配列
- * @param int $roomId roomId
- * @return string 取得したrole_keyを返す
- */
-//	public function getRoleInRoom(Model &$model, $calRoleAndPerm, $roomId = null) {
-//		if ($roomId === null) {
-//			$roomId = Current::read('Room.id');
-//		}
-//
-//		//copyを避け、参照代入にする
-//		//$readableRoomIds = &$calRoleAndPerm['readableRoomIds'];
-//		//$rooms = &$calRoleAndPerm['rooms'];
-//		//$roleOfRooms = &$calRoleAndPerm['roleOfRooms'];
-//		$roomInfos = &$calRoleAndPerm['roomInfos'];
-//
-//		$roleKey = '';
-//		if (empty($roomInfos)) {
-//			//ルームでの役割がない＝未ログイン
-//			return $roleKey;
-//		}
-//		if (!empty($roomInfos[$roomId])) {
-//			$roleKey = $roomInfos[$roomId]['role_key'];
-//		}
-//		return $roleKey;
-//	}
-
-/**
- * isContentCreatableAtRoleInRoom
- * 使用されていない
- *
- * ルーム管理＋カレンダー権限管理による、指定ルームにおける指定役割の人は、content_creatableかどうかの判断
- *
- * @param Model &$model 実際のモデル名
- * @param array $calRoleAndPerm カレンダー用役割・権限格納配列
- * @param string $roleKey roleKey
- * @param int $roomId roomId
- * @return string content_creatableならtrue。そうでないならfalseを返す。
- */
-//	public function isContentCreatableAtRoleInRoom(Model &$model,
-//		$calRoleAndPerm, $roleKey, $roomId = null) {
-//		return $this->isCommonAbleAtRoleInRoom($calRoleAndPerm, 'content_creatable', $roleKey, $roomId);
-//	}
-
-/**
- * isContentEditableAtRoleInRoom
- * 使用されていない
- *
- * ルーム管理＋カレンダー権限管理による、指定ルームにおける指定役割の人は、content_editableかどうかの判断
- *
- * @param Model &$model 実際のモデル名
- * @param array $calRoleAndPerm カレンダー用役割・権限格納配列
- * @param string $roleKey roleKey
- * @param int $roomId roomId
- * @return string content_editableならtrue。そうでないならfalseを返す。
- */
-//	public function isContentEditableAtRoleInRoom(Model &$model,
-//		$calRoleAndPerm, $roleKey, $roomId = null) {
-//		return $this->__isCommonAbleAtRoleInRoom($calRoleAndPerm, 'content_editable', $roleKey, $roomId);
-//	}
-
-/**
- * __isCommonAbleAtRoleInRoom
- * 使用されていない
- *
- * ルーム管理＋カレンダー権限管理による、指定ルームにおける指定役割の人は、content_xxxxxかどうかの判断
- *
- * @param array &$calRoleAndPerm カレンダー用役割・権限格納配列
- * @param string $able able(content_creatable, content_editable)が入る。
- * @param string $roleKey roleKey
- * @param int $roomId roomId
- * @return string content_xxxxxならtrue。そうでないならfalseを返す。
- */
-//	private function __isCommonAbleAtRoleInRoom(&$calRoleAndPerm, $able, $roleKey, $roomId = null) {
-//		if ($roomId === null) {
-//			$roomId = Current::read('Room.id');
-//		}
-//
-//		//copyを避け、参照代入にする
-//		//$readableRoomIds = &$calRoleAndPerm['readableRoomIds'];
-//		$rooms = &$calRoleAndPerm['rooms'];
-//		//$roleOfRooms = &$calRoleAndPerm['roleOfRooms'];
-//		//$roomInfos = &$calRoleAndPerm['roomInfos'];
-//
-//		$value = Hash::extract($rooms, '{n}.' . $roomId .
-//			'.BlockRolePermission.' . $able . '.' . $roleKey . '.value');
-//		if (!empty($value)) {
-//			$value = $value[0];
-//		} else {
-//			$value = 0;
-//		}
-//		return ($value) ? true : false;
-//	}
 
 /**
  * __getAllMemberRoleKey
