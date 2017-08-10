183cbc824c4995498bda6b5008743f422ac5cb85
RikaFujiwara <rika.fujiwara@allcreator.net>
2017-08-10 08:11:44 +0000

    fix phpunit error

--- Test/Case/AllCalendarsTest.php
+++ Test/Case/AllCalendarsTest.php
@@ -26,7 +26,7 @@ class AllCalendarsTest extends NetCommonsTestSuite {
  */
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
-		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
+		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
 		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 		return $suite;
 	}
