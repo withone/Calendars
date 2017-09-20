bc38f64674e53837958261120bf05d1a30e62d5b
kteraguchi <kteraguchi@commonsnet.org>
2017-09-20 10:43:28 +0900

    Fix for code coverage report

--- Test/Fixture/BlockMigration/SomeData/BlockMigrationSomeDataCalendarFixture.php
+++ Test/Fixture/BlockMigration/SomeData/BlockMigrationSomeDataCalendarFixture.php
@@ -21,31 +21,31 @@ class BlockMigrationSomeDataCalendarFixture extends BlockMigrationNoDataCalendar
  *
  * @var array
  */
- 	public $records = [
- 		[
- 			'id' => 1,
-	 		'block_key' => 'invalid001',
+	public $records = [
+		[
+			'id' => 1,
+			'block_key' => 'invalid001',
 			'created_user' => 1,
-	 		'created' => '2016-03-24 07:10:30',
-	 		'modified_user' => 1,
-	 		'modified' => '2016-03-24 07:10:30'
- 		],
- 		[
- 			'id' => 2,
- 			'block_key' => 'invalid001',
- 			'created_user' => 1,
- 			'created' => '2016-03-24 07:10:30',
- 			'modified_user' => 1,
- 			'modified' => '2016-03-24 07:10:30'
- 		],
- 		[
- 			'id' => 3,
- 			'block_key' => 'block002',
- 			'created_user' => 1,
- 			'created' => '2016-03-24 07:10:30',
- 			'modified_user' => 1,
- 			'modified' => '2016-03-24 07:10:30'
- 		],
-  	];
+			'created' => '2016-03-24 07:10:30',
+			'modified_user' => 1,
+			'modified' => '2016-03-24 07:10:30'
+		],
+		[
+			'id' => 2,
+			'block_key' => 'invalid001',
+			'created_user' => 1,
+			'created' => '2016-03-24 07:10:30',
+			'modified_user' => 1,
+			'modified' => '2016-03-24 07:10:30'
+		],
+		[
+			'id' => 3,
+			'block_key' => 'block002',
+			'created_user' => 1,
+			'created' => '2016-03-24 07:10:30',
+			'modified_user' => 1,
+			'modified' => '2016-03-24 07:10:30'
+		],
+	];
 
 }
